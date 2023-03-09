2c5d24f09566a96f02048468e798e8b45a87e500
soobbb <soob8437@gmail.com>
2023-03-09 09:44:49 +0900

    all

---
+++ 2.17 cal css/연습/1.js
@@ -0,0 +1,17 @@
+function letter(num, str){
+    // let num = 3;
+    // let str = "hello";
+
+    return str.slice(0, num);
+}
+
+function showTime(hour, min, sec) {
+    // TODO: 여기에 코드를 작성합니다.
+    let hour = 1;
+    let min = 2;
+    let sec = 30;
+    let c = `현재 시각은 ${hour}시 ${min}분 ${sec}초 입니다.`;
+  }
+
+word = "gi";
+console.log(word);
---
+++ 2.17 cal css/연습/2.js
@@ -0,0 +1,44 @@
+// function convertScoreToGradeWithPlusAndMinus(score) {
+//   // TODO: 여기에 코드를 작성합니다.
+// if (score < 0 || score > 100) 
+//     return "INVALID SCORE";
+
+// else if (score >= 90) {
+//     if(score <= 92)
+//         return "A-";
+//     else if(score <= 97)
+//         return "A";
+//     else
+//         return "A+";
+// } 
+// else if (score >= 80) {
+//     if (score <= 82) 
+//         return "B-";
+//     else if (score <= 87)
+//         return "B"
+//     else 
+//         return "B+";
+// } 
+// else if (score >= 70) {
+//     if (score <= 72) 
+//         return "C-";
+//     else if (score <= 77)
+//         return "C"
+//     else 
+//         return "C+";
+// }
+// else if (score >= 60) {
+//             if (score <= 62) 
+//                 return "D-";
+//             else if (score <= 67)
+//                 return "D"
+//             else 
+//                 return "D+";
+// }
+// else if (score < 60 && score >= 0) 
+// return "F";
+// }
+
+    // let hour = 0 >= hour && hout < 24;
+    // let minute = 0 >= minute && minute <= 60;
+    // let second = 0 >= second && second <= 60;
