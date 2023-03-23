5a0d075add03b6124bbeea57ac0ae688a5aebe8c
soobbb <soob8437@gmail.com>
2023-03-23 11:37:41 +0900

    React-Router

---
+++ 3.23 React Router/simpleroute/src/App.js
@@ -0,0 +1,45 @@
+import "./App.css";
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>{/* Link 컴포넌트를 이용하여 경로를 연결합니다 */}
+            </li>
+            <li>
+              <Link to="/mypage">MyPage</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} /> 
+					{/* 경로는 path로 컴포넌트는 element로 연결해 줍니다. */}
+          <Route path="/mypage" element={<MyPage />} /> 
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function MyPage() {
+  return <h1>MyPage</h1>;
+}
+
+function Dashboard() {
+  return <h1>Dashboard</h1>;
+}
+
+export default App;
